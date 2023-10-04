1b49f38d0efc289b3cb67a09941c5ac6c2018778
Mike Gustafson <93888281+mike-gustafson@users.noreply.github.com>
2023-10-03 21:13:38 -0700

    feat: scrolling works 100%.  Time to research level design options

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const platformAmount = 100
 let platforms = [];
 
 // Physics Variables
-let friction = 0.8;
+let friction = 0;
 let gravity = 1.3;
 let keys = {
     right: {
@@ -41,8 +41,14 @@ class Player {
         c.fillStyle = 'firebrick'
         c.fillRect(this.position.x, this.position.y, this.width, this.height);
     }
+    drawLabel() {
+        c.fillStyle = 'black';
+        c.font = '12px Arial';
+        c.fillText(`X: ${this.position.x} ${this.inLevelXPosition.x}`, this.position.x, this.position.y - 5);
+    }
     update() {
         this.create()
+        this.position.x = Math.round(this.position.x);
         this.position.y += this.velocity.y;
         this.velocity.y += gravity;
 
@@ -56,20 +62,29 @@ class Platform {
         this.width = Math.floor(Math.random() * (500 - 100) + 100)
         this.position = {
             x: Math.floor(Math.random() * levelWidth-this.width),
-            y: Math.floor((Math.random() * innerHeight)),
+            y: Math.floor((Math.random() * (innerHeight*.75)+(innerHeight/4)-30)),
         }
     }
     create() {
         c.fillStyle = 'white'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+    drawLabel() {
+        c.fillStyle = 'black';
+        c.font = '12px Arial';
+        c.fillText(`X: ${this.position.x}`, this.position.x, this.position.y - 5);
+    }
     update() {
         this.create()
+        this.position.x = Math.round(this.position.x);
+
         if (
             (player.position.x <= 100 && player.inLevelXPosition.x >= 100) ||
             (player.inLevelXPosition.x < levelWidth && player.position.x >= innerWidth / 2)
         ) {
-            this.position.x -= player.velocity.x;
+            if (player.velocity.x !== 0){
+                this.position.x -= player.velocity.x;
+            }
         }
     }
 }   
@@ -85,7 +100,7 @@ addEventListener('keydown', ({ keyCode }) => {
                 break;
         case 32:
             if (!player.jumping) {
-                player.velocity.y = -25;
+                player.velocity.y = -20;
                 player.jumping = true
             }
             break;
@@ -109,23 +124,25 @@ addEventListener('keyup', ({ keyCode }) => {
 const loop = function() {
     c.clearRect(0, 0, context.width, context.height);
     player.update();    
-    console.log(player.inLevelXPosition.x)
+    player.drawLabel()
+    console.log(player.inLevelXPosition.x-player.position.x)
     platforms.forEach(platform => {
         platform.update()
+        platform.drawLabel()
         if (
             player.position.y + player.height >= platform.position.y &&
             player.position.y <= platform.position.y + platform.height &&
-            player.inLevelXPosition.x + player.width >= platform.position.x &&
-            player.inLevelXPosition.x <= platform.position.x + platform.width
+            player.position.x + player.width >= platform.position.x &&
+            player.position.x <= platform.position.x + platform.width
         ) {
             player.jumping = false;
             player.position.y = platform.position.y - player.height;
-            player.velocity.y = 0;
+            player.velocity.y = 0
         }
     });
-
-    // syncs both position values at an appropriate time, usefull for running back and forth in a level
-    if  ((player.position.x < innerWidth/2) && (player.position.x > innerWidth/4) &&
+console.log(player.position.x)
+console.log(player.inLevelXPosition.x)
+    if  ((player.position.x < innerWidth/2) && (player.position.x > playerStartingXPosition) &&
         (player.inLevelXPosition.x <innerWidth/2) && (player.inLevelXPosition.x > innerWidth/4)) {
             player.position.x = player.inLevelXPosition.x
         }
